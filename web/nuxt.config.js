d0f4bf6b3a486b240485e61b61ef85dae105f596
Julian Schmidt <mail@julianschmidt.me>
2022-02-02 17:41:36 +0100

    web: sanity & core dependencies added, env example added

--- web/nuxt.config.js
+++ web/nuxt.config.js
@@ -1,40 +1,59 @@
 export default {
-  // Target: https://go.nuxtjs.dev/config-target
-  target: 'static',
+	// Target: https://go.nuxtjs.dev/config-target
+	target: 'static',
 
-  // Global page headers: https://go.nuxtjs.dev/config-head
-  head: {
-    title: 'Sanity Nuxt 2 Starter',
-    htmlAttrs: {
-      lang: 'de',
-    },
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' },
-    ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
-  },
+	// Global page headers: https://go.nuxtjs.dev/config-head
+	head: {
+		title: 'Sanity Nuxt 2 Starter',
+		htmlAttrs: {
+			lang: 'de',
+		},
+		meta: [
+			{ charset: 'utf-8' },
+			{
+				name: 'viewport',
+				content: 'width=device-width, initial-scale=1',
+			},
+			{ hid: 'description', name: 'description', content: '' },
+			{ name: 'format-detection', content: 'telephone=no' },
+		],
+		link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+	},
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+	router: {
+		trailingSlash: false,
+	},
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+	sitemap: {
+		trailingSlash: false,
+	},
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
+	// Global CSS: https://go.nuxtjs.dev/config-css
+	css: [],
 
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-    // https://go.nuxtjs.dev/eslint
-    '@nuxtjs/eslint-module',
-  ],
+	// Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+	plugins: [],
 
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [],
+	// Auto import components: https://go.nuxtjs.dev/config-components
+	components: true,
 
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
+	// Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+	buildModules: [
+		// https://go.nuxtjs.dev/eslint
+		'@nuxtjs/eslint-module',
+		'@nuxtjs/sanity/module',
+	],
+
+	// Modules: https://go.nuxtjs.dev/config-modules
+	modules: [],
+
+	// Build Configuration: https://go.nuxtjs.dev/config-build
+	build: {},
+
+	sanity: {
+		projectId: process.env.SANITY_PROJECT_ID,
+		dataset: process.env.SANITY_DATASET,
+		apiVersion: '2022-02-02',
+		useCdn: true,
+	},
 }
